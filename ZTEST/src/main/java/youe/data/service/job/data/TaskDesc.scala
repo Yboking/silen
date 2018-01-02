85babc1a9a6bbf3ce0595b896d8ba24bc9876045
kingw <king_ybw@163.com>
2018-01-02 17:50:02 +0800

    set default task id

--- ZTEST/src/main/java/youe/data/service/job/data/TaskDesc.scala
+++ ZTEST/src/main/java/youe/data/service/job/data/TaskDesc.scala
@@ -1,4 +1,4 @@
 package youe.data.service.job.data
 
-case class TaskDesc(id: Long, ttype: String, command: Array[String], source: Int, target: Int)
+case class TaskDesc(id: Long = 0, ttype: String, command: Array[String], source: Int, target: Int)
 
