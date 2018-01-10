3a10f1c32289b0f8db330743a76ff707a069352a
kingw <king_ybw@163.com>
2018-01-10 11:30:58 +0800

    add scheduler object

--- scheduler.ui/src/main/java/silen/scheduler/ui/actions/SchedulerAction.java
+++ scheduler.ui/src/main/java/silen/scheduler/ui/actions/SchedulerAction.java
@@ -2,9 +2,16 @@ package silen.scheduler.ui.actions;
 
 import org.springframework.stereotype.Controller;
 
+import silen.scheduler.service.job.MasterSystem;
+
 @Controller
 public class SchedulerAction {
 
 	
+	{
+		
+		MasterSystem.main(null); 	
+		
+	}
 	
 }
