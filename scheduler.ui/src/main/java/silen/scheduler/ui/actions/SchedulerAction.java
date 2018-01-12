01c09fe10e454ff62e42fa8663f8f6791a9dab84
kingw <king_ybw@163.com>
2018-01-12 14:39:38 +0800

    fix bug :  userAppConfig nullpoint

--- scheduler.ui/src/main/java/silen/scheduler/ui/actions/SchedulerAction.java
+++ scheduler.ui/src/main/java/silen/scheduler/ui/actions/SchedulerAction.java
@@ -1,17 +1,36 @@
 package silen.scheduler.ui.actions;
 
+import javax.annotation.Resource;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 
 import silen.scheduler.service.job.MasterSystem;
+import silen.scheduler.ui.util.ConfigKeys;
+import silen.scheduler.ui.valuebean.UserAppConfig;
 
 @Controller
 public class SchedulerAction {
+	  private Logger logger = LogManager.getLogger(this.getClass());
 
-	
-	{
-		
-		MasterSystem.main(null); 	
-		
+	private UserAppConfig uac;
+
+	@Resource
+	public void setWebConfig(UserAppConfig userAppConfig) {
+		this.uac = userAppConfig;
+		init();
 	}
-	
+
+	private void init() {
+
+		
+		logger.info(" UserAppConfig userAppConfig : "+ uac.getUserExtLibs());
+
+
+		MasterSystem.main(new String[] { String.format("%s=%s",
+				ConfigKeys.USER_EXT_LIBS, uac.getUserExtLibs()) });
+
+	}
+
 }
--- scheduler.ui/src/main/java/silen/scheduler/ui/valuebean/UserAppConfig.java
+++ scheduler.ui/src/main/java/silen/scheduler/ui/valuebean/UserAppConfig.java
@@ -6,15 +6,24 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserAppConfig {
 
-	
 	private String userExtLibs;
+
+	private String version;
+
+	public String getVersion() {
+		return version;
+	}
+
+	@Value("${version}")
+	public void setVersion(String version) {
+		this.version = version;
+	}
 
 	public String getUserExtLibs() {
 		return userExtLibs;
 	}
 
-	
-	 @Value("${user.extlibs}")
+	@Value("${user.extlibs}")
 	public void setUserExtLibs(String userExtLibs) {
 		this.userExtLibs = userExtLibs;
 	}
