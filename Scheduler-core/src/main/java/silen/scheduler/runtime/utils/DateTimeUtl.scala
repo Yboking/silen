b1b24f116c5a9b438514dbc2cd882e188542c19e
kingw <king_ybw@163.com>
2018-01-22 18:01:17 +0800

    DateTime util

---
+++ Scheduler-core/src/main/java/silen/scheduler/runtime/utils/DateTimeUtl.scala
@@ -0,0 +1,23 @@
+package silen.scheduler.runtime.utils
+
+import java.text.SimpleDateFormat
+import java.util.Date
+
+object DateTimeUtl {
+
+  val DEFAULT_DATETIME_FMT = "yyyy/MM/dd HH:mm:ss"
+
+  var formater = new SimpleDateFormat(DEFAULT_DATETIME_FMT)
+
+  def currentDateTime(fmt: String = null) = {
+
+    if (fmt != null) {
+
+      formater = new SimpleDateFormat(fmt)
+
+    }
+
+    formater.format(new Date())
+
+  }
+}
