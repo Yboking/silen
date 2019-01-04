0c5c60c752b9f8e83bc6480aa431f8b7fb92d614
yboking <king_ybw@163.com>
2019-01-04 19:23:05 +0800

    test cart

--- silen-ml/src/test/java/TestCart.scala
+++ silen-ml/src/test/java/TestCart.scala
@@ -1,24 +1,14 @@
 import silen.ml.classification.CartTree
 import silen.ml.data.TrainSet
 
-import scala.collection.mutable
-
 object TestCart {
 
   def main(args: Array[String]): Unit = {
 
+    val train = TrainSet.fromFile("data/iris.csv")
 
-
-
-   println(Array(5.1,3.8,1.6,0.2).eq(Array(5.1,3.8,1.6,0.2)))
-   println(Array(5.1,3.8,1.6,0.2) == (Array(5.1,3.8,1.6,0.2)))
-   println(Array(5.1,3.8,1.6,0.2).equals(Array(5.1,3.8,1.6,0.2)))
-//    val train = TrainSet.fromFile("data/iris.csv")
-//
-//    val cart = new CartTree()
-//    val tree = cart.fit(train)
-//
-//
-//    println(train)
+    val cart = new CartTree()
+    val tree = cart.fit(train)
+    println(train)
   }
 }
